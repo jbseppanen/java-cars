41e810d3ccd7479e2ca7b7f4d56e57932529d785
Jacob Seppanen <jbseppanen@gmail.com>
2019-02-15 08:32:13 -0800

    Update JavaCarsApplication class to include the necessary beans.

--- src/main/java/com/lambdaschool/javacars/JavaCarsApplication.java
+++ src/main/java/com/lambdaschool/javacars/JavaCarsApplication.java
@@ -1,13 +1,53 @@
 package com.lambdaschool.javacars;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JavaCarsApplication {
 
+    public static final String EXCHANGE_NAME = "Cars";
+    public static final String QUEUE_NAME_LOG = "Log";
+
+
     public static void main(String[] args) {
         SpringApplication.run(JavaCarsApplication.class, args);
+    }
+
+
+    @Bean
+    public TopicExchange appExchange() {
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Queue appQueue() {
+        return new Queue(QUEUE_NAME_LOG);
+    }
+
+    @Bean
+    public Binding declareBinding() {
+        return BindingBuilder.bind(appQueue()).to(appExchange()).with(QUEUE_NAME_LOG);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 
 }
